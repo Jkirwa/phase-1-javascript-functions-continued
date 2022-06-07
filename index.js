e40e811cb1982d4fb513484ab6aa43c6d2881938
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-07 20:50:42 +0300

    commit tree 4277878c64738a53f8f85e9777eb47df22621ceb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun (fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+    }
+    saturdayFun();
+    saturdayFun("bathe my dog");
+
+
+    let mondayWork = function(doWhat="go to the office"){
+        return `This Monday, I will ${doWhat}.`
+    }
+    mondayWork()
+    mondayWork("work from home")
+        
+
+    function wrapAdjective(adjective="*"){
+        return function(myPar="awesome" ){
+            return `You are ${adjective}${myPar}${adjective}!`;
+        }
+    }
+    wrapAdjective()("a hard worker");
+    wrapAdjective("||")("a dedicated programmer");
+    
+    
+
+
+    
